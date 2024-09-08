bf91642d952300f9eb56745b3f8b5c2aebf1fc69
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2024-09-08 12:24:03 +0100

    menus.json for data updated. function for menu generation with DOM on index.html created and called

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,22 @@
 const response = await fetch('assets/js/menus.json'); // Utilisation de 'await' car fetch() est asynchrone
-const menus = await response.json(); // Utilisation de .json() pour extraire les données
+let menus = await response.json(); // Utilisation de .json() pour extraire les données
 console.log(menus);
 
-const menuContainer = document.querySelector('.menu-container');
+let menuContainer = document.querySelector('.menu-container');
 
-function genererMenu(menu){
-    for (let i = 0; i < menu.lenght; i++) {
+function genererMenu(menus){
+    for (let i = 0; i < menus.length; i++) {
         const menuItem = document.createElement('div');
         menuItem.classList.add('col-12','col-md-6');
         menuItem.innerHTML = `
             <div class="container">
                 <div class="row">
                     <div class="col-12 d-flex mt-2 gap-2">
-                        <img src="${menu.image}" width="150" class="img-fluid" alt="">
+                        <img src="${menus[i].image}" width="150" class="img-fluid" alt="">
                         <div>
-                            <h3 class="fw-semibold">${menu.nom}</h3>
-                            <hr>
-                            <p class="fs-4"><span>Prix : </span>${menu.prix}</p>
+                            <h3 class="fw-semibold">${menus[i].nom}</h3>
+                            <hr class="w-100">
+                            <p class="fs-4"><span>Prix : </span>${menus[i].prix}</p>
                         </div>
                     </div>
                 </div>
@@ -25,5 +25,5 @@ function genererMenu(menu){
         menuContainer.appendChild(menuItem);
     }
 }
-document.addEventListener('DOMContentLoaded', genererMenu(menus));
 
+genererMenu(menus);
