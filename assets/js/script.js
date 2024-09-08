623f1424ad93de85cec35039cbc52e70bf1c03e0
NicoSelomin <vodounnounicodemeselomin@gmail.com>
2024-09-08 16:42:37 +0100

    style for responsive added in style.css, img for menu éléments added and script updated for star(note) showing

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,13 @@
 const response = await fetch('assets/js/menus.json'); // Utilisation de 'await' car fetch() est asynchrone
 let menus = await response.json(); // Utilisation de .json() pour extraire les données
 console.log(menus);
+const joursFrancais = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+
+const today = new Date(); // Obtenir la date actuelle
+const dayName = joursFrancais[today.getDay()]; // Obtenir le jour de la semaine
+
+console.log(dayName); // Affiche le jour actuel en français
+
 
 let menuContainer = document.querySelector('.menu-container');
 
@@ -12,16 +19,38 @@ function genererMenu(menus){
             <div class="container">
                 <div class="row">
                     <div class="col-12 d-flex mt-2 gap-2">
-                        <img src="${menus[i].image}" width="150" class="img-fluid" alt="">
+                        <img src="${menus[i].image}"class="menu-img img rounded img-thumbnail" alt="">
                         <div>
                             <h3 class="fw-semibold">${menus[i].nom}</h3>
                             <hr class="w-100">
-                            <p class="fs-4"><span>Prix : </span>${menus[i].prix}</p>
-                        </div>
+                            <div class="d-flex flex-column flex-md-row gap-2 align-content-center">
+                                <p class="fs-4"><span>Prix : </span>${menus[i].prix}</p>
+                                <p id="avis-${i}"></p>
+                                <button class="btn btn-warning mb-5 btn-sm fw-semibold">Commander</button>
+                            </div>
+                        </div> 
                     </div>
                 </div>
             </div>
         `;
+        // Sélection de le paragraph où les étoiles seront affichées
+        const avisContainer = menuItem.querySelector(`#avis-${i}`);
+
+        // Boucle pour afficher les étoiles selon la note (avis)
+        for (let j = 0; j < Math.floor(menus[i].avis); j++) {
+            avisContainer.innerHTML += `<i class="bi bi-star-fill text-warning"></i>`;
+        }
+
+        // Si l'avis contient une demi-étoile (avis décimal), ajouter une demi-étoile
+        if (menus[i].avis % 1 !== 0) {
+            avisContainer.innerHTML += `<i class="bi bi-star-half text-warning"></i>`;
+        }
+
+        // Compléter avec des étoiles blanches (pour un total de 5 étoiles)
+        const totalStars = Math.ceil(menus[i].avis); // Total filled or half-filled stars
+        for (let j = totalStars; j < 5; j++) {
+            avisContainer.innerHTML += `<i class="bi bi-star text-warning"></i>`;
+        }
         menuContainer.appendChild(menuItem);
     }
 }
